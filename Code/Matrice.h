5d1ba3c82fabcfa85a977bd4c38d391390e94fc3
Geoffrey HAON <Geoffrey HAON>
2016-10-03 17:24:53 +0200

    Type Matrice

--- Code/Matrice.h
+++ Code/Matrice.h
@@ -3,18 +3,18 @@ struct matrice{
 
   unsigned int h;
   unsigned int l;
-  Point* points;
+  unsigned char* points;
 
 };
 
 matrice MatAlloc(int h,intl); // création d’une nouvelle matrice h * l initialisée à 0
 
-MatFree : libération éventuelle de la mémoire utilisée
+MatFree // libération éventuelle de la mémoire utilisée
 
-MatVal : renvoie la valeur entière matrice[x,y]
+char MatVal(int x,int y); // renvoie la valeur entière matrice[x,y]
 
-MatSet : stocke une valeur entière dans matrice[x,y]
+void MatSet(int x,int y); // stocke une valeur entière dans matrice[x,y]
 
-MatSauve : enregistre une matrice dans un fichier
+void MatSauve(matrice m); // enregistre une matrice dans un fichier
 
-MatLit : lit une matrice dans un fichier
+matrice MatLit(char* file); // lit une matrice dans un fichier
