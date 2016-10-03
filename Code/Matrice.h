2ac85575c607eb2938dc56c6d7627e50c52d1db1
Geoffrey HAON <Geoffrey HAON>
2016-10-03 17:03:02 +0200

    Ajout files

---
+++ Code/Matrice.h
@@ -0,0 +1,18 @@
+
+struct matrice{
+
+
+
+}*matrice;
+
+matrice MatAlloc(h*l) : création d’une nouvelle matrice h * l initialisée à 0
+
+MatFree : libération éventuelle de la mémoire utilisée
+
+MatVal : renvoie la valeur entière matrice[x,y]
+
+MatSet : stocke une valeur entière dans matrice[x,y]
+
+MatSauve : enregistre une matrice dans un fichier
+
+MatLit : lit une matrice dans un fichier
